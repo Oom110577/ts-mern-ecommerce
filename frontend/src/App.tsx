851e6976822a88c227e3a4c110a66c91f8f14980
oom115077 <chaiyathon.k@mail.bbvc.ac.th>
2024-05-22 11:27:41 +0700

    Add-react-bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,44 @@
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap"
 import {sampleProducts}  from "./data"
-import './App.css'
 
 function App() {
-
   return (
-    <>
-    <header>A.Win Shop</header>
+    <div className="d-flex flex-column vh-100">
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" >
+        <Container>
+          <Navbar.Brand>
+            AJ.Win shop
+          </Navbar.Brand>
+        </Container>
+        <Nav>
+          <a href="/cart" className="nav-link">cart</a>
+          <a href="/signin" className="nav-link">Sing In</a>
+        </Nav>
+      </Navbar>
+    </header>
     <main>
-      <ul>
+      <Container className="mt-3">
+        <Row>
         {
           sampleProducts.map((product)=>(
-            <li key={product.slug}>
+            <Col key={product.slug} sm={6} md={4} lg={3}>
               <img  src={product.image} alt={product.image} className="product-image" />
               <h2>{product.name}</h2>
               <p>${product.price}</p>
-            </li>
+            </Col>
           ))}
-      </ul>
+      </Row>
+      </Container>
     </main>
-    <footer>All right reserved</footer>
-    </>
+   
+    <footer>
+      <div className="text-center">
+      All right reserved
+      </div>
+      
+      </footer>
+    </div>
   )
 }
 

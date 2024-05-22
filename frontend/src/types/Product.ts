df1cb0d406d630d32c4b1c6db0876277056fffcb
oom115077 <chaiyathon.k@mail.bbvc.ac.th>
2024-05-22 10:16:59 +0700

    List-Product

---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,14 @@
+
+export type Product = {
+    name: string
+    slug: string // แทนID
+    image: string
+    category: string
+    brand: string
+    price: number
+    countInStock: number
+    description: string
+    rating: number
+    numReviews: number
+  }
+  

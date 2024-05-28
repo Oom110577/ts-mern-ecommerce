733e3714b71f79ed37a7ac220c3454af5fed5ba6
oom115077 <chaiyathon.k@mail.bbvc.ac.th>
2024-05-28 09:26:27 +0700

    Create-Product-Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,24 +9,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css' 
-import HomePage from "./pages/HomePage.tsx";
 import ProductPage from "./pages/ProductPage.tsx";
-import axios from "axios";
-
-axios.defaults.baseURL =
-process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
+import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import HomePage from "./pages/HomePage.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
-      <Route index={true} element={<HomePage/>} />
+      <Route index={true} element={<HomePage />} />
       <Route path='product/:slug' element={<ProductPage/>}  />
     </Route>
   )
 );
 
+const queryClient = new QueryClient()
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+  <HelmetProvider>
+  <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+   </HelmetProvider>
   </React.StrictMode>,
 )

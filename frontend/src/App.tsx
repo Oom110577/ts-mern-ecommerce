1783e23bef7fd2a2cbc989aaa5c44876697508fc
oom115077 <chaiyathon.k@mail.bbvc.ac.th>
2024-05-29 13:43:53 +0700

    Cart-In-Product-Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react'
 import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Store } from './Store'
 
 function App() {
@@ -8,25 +11,26 @@ function App() {
     state: { mode, cart },
     dispatch,
   } = useContext(Store)
-
   useEffect(() => {
     document.body.setAttribute('data-bs-theme', mode)
   }, [mode])
   const switchModeHandler = () => {
     dispatch({ type: 'SWITCH_MODE' })
   }
+
   return (
     <div className="d-flex flex-column vh-100">
+      <ToastContainer position="bottom-center" limit={1} />
       <header>
         <Navbar expand="lg">
           <Container>
-            <Navbar.Brand>tsamazona</Navbar.Brand>
-          </Container>
+          <LinkContainer to="/">
+              <Navbar.Brand>tsamazona</Navbar.Brand>
+            </LinkContainer>         </Container>
           <Nav>
             <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-
             <Link to="/cart" className="nav-link">
               Cart
               {cart.cartItems.length > 0 && (
